d01e44297607a3e5ca92ce5503387a8da3cc2dc2
Jayson Lennon <jayson@jaysonlennon.dev>
2023-07-06 13:18:48 -0700

    sp

--- demo/unions/unions.ts
+++ demo/unions/unions.ts
@@ -2,7 +2,7 @@
 
 // Union types allows you to declare a variable or parameter that can hold
 // multiple types of value and are declared using the pipe symbol (|) between
-// the types. Union types can be useful when you want a something to accept
+// the types. Union types can be useful when you want something to accept
 // multiple types of input.
 //
 // Useful links:

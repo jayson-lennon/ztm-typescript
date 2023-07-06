e383f73ae564cd57efcd1f247962a1d86740a35c
Jayson Lennon <jayson@jaysonlennon.dev>
2023-07-06 13:40:55 -0700

    grammar

--- demo/async-await/async-await.ts
+++ demo/async-await/async-await.ts
@@ -5,9 +5,10 @@ import { strict as assert } from "assert";
 // The `async` keyword used with a function or closure creates an asynchronous
 // context. The `await` keyword can be used inside an asynchronous context to
 // wait for a `Promise` to resolve before moving on to the next line of code.
-// When the promise resolves, the value is returned and assigned to the
-// variable on the left side of the `=` sign. This makes it easier to work with
-// asynchronous code as you can write code in a more sequential way.
+// While waiting, other code outside the function will execute. When the
+// promise resolves, the value is returned and assigned to the variable on the
+// left side of the `=` sign. This makes it easier to work with asynchronous
+// code as you can write code in a more sequential way.
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function

f13bb75e744f25b5e649b5625fca101d75686556
mo7tr <mo7tr@gmail.com>
2023-09-26 17:30:36 +0100

    arithmetic, increment

--- demo/arithmetic/arithmetic.ts
+++ demo/arithmetic/arithmetic.ts
@@ -2,3 +2,31 @@
 import { strict as assert } from "assert";
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+const add = 1 + 1; // 2
+assert.equal(add, 2);
+
+const subtract = 2 - 1; // 1
+assert.equal(subtract, 1);
+
+const multiply = 3 * 3; // 9
+assert.equal(multiply, 9);
+
+const divide = 8 / 2; // 4
+assert.equal(divide, 4);
+
+// You won't get a divide by zero error.
+const infinity = 1 / 0; // Infinity
+assert.equal(infinity, Infinity);
+
+const notANumber = 0 / 0; // NaN
+assert(isNaN(notANumber));
+
+const remainder = 10 % 3; // 1
+assert.equal(remainder, 1);
+
+const negate = -remainder; // -1
+assert.equal(negate, -1);
+
+const exponent = 2 ** 3; // 8
+assert.equal(exponent, 8);
--- demo/increment/increment.ts
+++ demo/increment/increment.ts
@@ -5,5 +5,36 @@ import { strict as assert } from "assert";
 // common that there is an operator dedicated to just incrementing numbers.
 // However, it does come with a few caveats to be aware of.
 
-// Useful links:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// Bugs can occur when assigning and postfix or prefix incrementing are
+// combined:
+let increment = 2;
+const two = increment++; // still 2
+// Here's what happens above:
+//   const two = increment;
+//   increment = increment + 1;
+assert.equal(two, 2);
+
+let increment_bis = 2;
+const three = ++increment_bis; // 3
+assert.equal(three, 3);
+// Here's what happens above:
+//   increment_bis = increment_bis + 1;
+//   const three = increment_bis;
+
+// 'Postfix' will return the original value then update the variable.
+// 'Prefix' will update the original variable and then
+// return the updated value.
+
+// Use 'add assign' instead to avoid bugs...
+let addAssign = 0;
+addAssign += 1; // 1
+assert.equal(addAssign, 1);
+// ... which is the same as this:
+let moreAdding = 0;
+moreAdding = moreAdding + 1;
+assert.equal(moreAdding, 1);
+
+// You can use other arithmetic operators with 'assign':
+let subAssign = 1;
+subAssign -= 1; // 0
+assert.equal(subAssign, 0);

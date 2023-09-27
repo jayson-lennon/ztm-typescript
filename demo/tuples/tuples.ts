4dd911c45076419ee90f5cf8d0fb69a73c0ccc46
mo7tr <mo7tr@gmail.com>
2023-09-27 15:46:29 +0100

    tuples

--- demo/tuples/tuples.ts
+++ demo/tuples/tuples.ts
@@ -7,6 +7,30 @@ import { strict as assert } from "assert";
 // representing coordinates, storing key-value pairs, or returning multiple
 // values from a function. Since they are type-checked, TypeScript can ensure
 // that the values in the tuple are correct at compile time.
-//
-// Useful links:
-// https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+
+type Title = string;
+type PublishYear = number;
+
+// Declare a tuple type
+let book: [Title, PublishYear];
+
+// Initialize a tuple
+book = ["sample", 1980];
+
+assert.equal(book[0], "sample");
+assert.equal(book[1], 1980);
+
+function coord(): [number, number] {
+  return [3, 5];
+}
+
+const coordinate = coord();
+assert.equal(coordinate[0], 3);
+assert.equal(coordinate[1], 5);
+
+const [x, y] = coord();
+assert.equal(x, 3);
+assert.equal(y, 5);
+
+//a rray of tuples
+const multi: [number, number][] = [coord(), [1, 1], [0, 0], [1, 9]];

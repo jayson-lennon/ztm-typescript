52f60ecf7435024df4929f5af872150c941fdf19
mo7tr <mo7tr@gmail.com>
2023-09-27 10:57:42 +0100

    type-alias

--- demo/type-alias/type-alias.ts
+++ demo/type-alias/type-alias.ts
@@ -6,6 +6,26 @@ import { strict as assert } from "assert";
 // then be used as types for variables, function parameters, and return types.
 // Type aliases offer a way to make your code more readable and maintainable by
 // providing descriptive names for complex types.
-//
-// Useful links:
-// https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-aliases
+
+// We can use a type alias as a way to make our code clearer:
+type PersonName = string;
+
+// Type aliases are _just_ aliases. This means anywhere we see the
+// type `PersonName`, we can use a `string` in it's place.
+// These two are the same type (string):
+const myName: PersonName = "Jayson";
+const alsoMyName: string = "Jayson";
+
+// example of a function with poor naming:
+function print(thing: string) {
+  console.log(`Name is ${thing}`);
+}
+
+print("coucou");
+
+// using proper naming and type aliases:
+function displayGreeting(name: PersonName) {
+  console.log(`Name is ${name}`);
+}
+
+displayGreeting("john");

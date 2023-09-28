5de5a9b0e0b7a1b322176f70b62fd9297d14f54b
mo7tr <mo7tr@gmail.com>
2023-09-28 15:02:51 +0100

    todo app, classes

--- demo/classes/classes.ts
+++ demo/classes/classes.ts
@@ -5,6 +5,108 @@ import { strict as assert } from "assert";
 // and behavior and can be used to create instances of objects with predefined
 // properties and methods. Classes can be extended and inherited, allowing for
 // the creation of complex object hierarchies.
-//
-// Useful links:
-// https://www.typescriptlang.org/docs/handbook/2/classes.html
+
+// To crete a class definition, we use the `class` keyword followed
+// by the name of the object and then curly braces:
+class Empty {}
+
+// Classes can have 'properties' and they must have default values set.
+// These defaults will be used when the objet is created.
+class Color {
+  // Always include the type annotation. It's optional, but
+  // readability will be enhanced with the type specified.
+  r: number = 0;
+  g: number = 0;
+  b: number = 0;
+}
+
+// We can crete a new object using the `new` keyword. Creating
+// a new object is called 'instantiation'.
+const c = new Color(); // Color { r: 0, g: 0, b: 0 }
+
+// We use dot notation to set values within the object:
+c.r = 255; // Color { r: 255, g: 0, b: 0 }
+
+// And we can read values using dot notation as well:
+const redValue = c.r;
+
+console.log("c", c);
+
+// Sometimes we may want to perform additional actions
+// when instantiating a new class. To do so, we can use
+// a class constructor:
+class Dimension {
+  // We aren't specifying the defaults directly on the properties.
+  // We will set the defaults in the constructor instead.
+  length: number;
+  width: number;
+  height: number;
+
+  // Class constructor is a function that runs each time we create
+  // a Dimension using the `new` keyword. Constructors always return
+  // the type they are defined in (`Dimension` in this case). So
+  // there is no need for the return type annotation.
+  constructor() {
+    // Each time we make a new Dimension, this message will be logged.
+    console.log("created a new default Dimension");
+
+    // Since we didn't specify the defaults, we _must_ do so here. Failure
+    // to specify defaults will result in an error.
+    this.length = 1;
+    this.width = 1;
+    this.height = 1;
+  }
+}
+
+const one = new Dimension(); // 'created a new Dimension'
+console.log("one", one);
+
+class Point {
+  x: number;
+  y: number;
+
+  // We can also write the constructor with function parameters. This
+  // allows us to easily set values during creation.
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // We can create 'class methods' which can be called using dot notation.
+  // This method moves the point by the provided amount.
+  translate(x: number, y: number) {
+    this.x += x;
+    this.y += y;
+  }
+}
+
+const a = new Point(5, 5); // Point { x: 5, y: 5 }
+console.log("a", a);
+a.translate(-5, -5); // Point { x: 0, y: o }
+console.log("a after translate method", a);
+assert.deepEqual([a.x, a.y], [0, 0]);
+
+class Point_bis {
+  x: number;
+  y: number;
+  // z: number; will be created with public keyword
+
+  // We can also write in the constructor, function parameters with the public keyword,
+  // this will instatntiate a new property, z in this case.
+  constructor(x: number, y: number, public z: number) {
+    this.x = x;
+    this.y = y;
+    //this.z = z; will be instantiated through public keyword in constructor
+  }
+
+  // We can create 'class methods' which can be called using dot notation.
+  // This method moves the point by the provided amount.
+  translate(x: number, y: number) {
+    this.x += x;
+    this.y += y;
+  }
+}
+
+const b = new Point_bis(1, 1, 7);
+console.log("b", b);
+assert.deepEqual([b.x, b.y, b.z], [1, 1, 7]);

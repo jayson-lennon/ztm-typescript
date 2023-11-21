8b62022a01c011c3f0672827cbf78e42c51e2b2d
njordr <njordrenterprises@gmail.com>
2023-11-21 04:07:26 +0000

    1st commit

--- demo/boolean-logic/boolean-logic.ts
+++ demo/boolean-logic/boolean-logic.ts
@@ -1,6 +1,31 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+
+const yes = true;
+const no = false;
+
+const age = 18;
+const canPurchase = age >= 18;
+assert.equal(canPurchase, true);
+
+const denyPurchase = age < 18;
+assert.equal(denyPurchase, false);
+
+const two = 2;
+const isThree = (1 + two) === 3;
+assert.equal(isThree, true);
+
+const isNotThree = (1 + two) !== 10;
+assert.equal(isNotThree, true);
+
+// < 
+// <=
+// >
+// >=
+// ===
+// !==
+//check
 // Boolean logic is a fundamental concept that involves evaluating true/false
 // statements. It is a system of logical thought that uses true and false
 // values to make deductions or draw conclusions. Boolean logic is used to
--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -1,5 +1,48 @@
 /* eslint-disable */
 
+const courseName = "typescript";
+const courseName2 = 'typescript';
+
+const courseName3 = `typescript`;
+
+const amount = 10;
+const fraction = 10.5;
+
+const oneThousand = 1e3;
+
+const allPermissions = 0o777;
+
+const hexByte = 0xff;
+
+const binary = 0b01001;
+
+const bigInt = 9000n
+
+const yes = true; 
+const no = false;
+
+//completely missing
+const missing = undefined;
+//we know its missing
+const empty = null;
+
+let someNum = 0;
+someNum = 1;
+someNum = 2;
+
+//curly braces for shadow coding for testing
+{
+    let someNum = 5;
+}
+
+ let hello;
+
+hello = "hi";
+hello = "bye";
+hello = "hello";
+hello = "world";
+
+
 // A variable is a named memory location that can hold a value. Variables can
 // be used to store a wide range of data types, such as numbers, strings, and
 // arrays. A variable is declared by specifying its name, data type, and

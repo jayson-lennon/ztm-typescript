ae4e68814164af51a155bb2e9e64f824cd2644d3
Jayson Lennon <jayson@jaysonlennon.dev>
2024-08-11 09:37:06 -0700

    add union types ex 1

---
+++ exercise/unions/1.ts
@@ -0,0 +1,19 @@
+// You are working on a task management system where tasks can have different
+// statuses.Implement a function that logs a message based on the type of task
+// status, which can be either a string(status message) or a number(status
+// code).
+//
+// Requirements:
+// - Create a function called `logTaskStatus` that:
+//   - Takes status, which can be either a `string` or a `number`.
+//   - If status is a `string`, print "Task status message: " followed by the
+//     status message.
+//   - If status is a `number`, print "Task status code: " followed by the
+//     status code.
+
+
+
+logTaskStatus("Completed"); // Output: Task status message: Completed
+logTaskStatus(200);         // Output: Task status code: 200
+logTaskStatus("In Progress"); // Output: Task status message: In Progress
+logTaskStatus(404);         // Output: Task status code: 404

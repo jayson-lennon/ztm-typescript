4948bcf1d161e152865f8b6f6f903a14f82dfe62
Jayson Lennon <jayson@jaysonlennon.dev>
2024-09-13 23:15:14 -0700

    update tailwind config

--- app/submit-form/tailwind.config.js
+++ app/submit-form/tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,njk,html}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: [
       {
         dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          // ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
           primary: "rgb(56, 189, 248)",
         },
       },

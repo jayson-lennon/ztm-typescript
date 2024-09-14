4098e9a496004f37c47c309dda173e5da9b03987
Jayson Lennon <jayson@jaysonlennon.dev>
2024-09-13 23:14:44 -0700

    update tailwind config

--- app/weather-ui/tailwind.config.js
+++ app/weather-ui/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,njk,html}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };

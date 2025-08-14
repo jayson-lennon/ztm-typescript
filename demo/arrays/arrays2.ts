dd3f063d97909aaa78f800c4212a03b68c391321
Jayson Lennon <jayson@jaysonlennon.dev>
2025-08-13 19:22:17 -0700

    remove stuff

--- demo/arrays/arrays2.ts
+++ demo/arrays/arrays2.ts
@@ -1,9 +1,3 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
-const numbers = [1, 2, 3];
-
-let letters = ["a", "b", "c"];
-
-// error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'
-// letters.push(1);

ddac53220a4d3f3bb2fe345d26fdf267d81846a1
Jayson Lennon <jayson@jaysonlennon.dev>
2025-08-15 11:38:41 -0700

    clearity

--- demo/interfaces/properties-nested.ts
+++ demo/interfaces/properties-nested.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
-// Define a nested interface for an address
+// Define an interface for an address
 interface Address {
   street: string;
   city: string;
